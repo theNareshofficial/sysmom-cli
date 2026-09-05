43b82f48dad90dfaec44d9f9b7051128ea2c3da0
Narehs R <theNareshofficial>
2026-09-05 12:27:57 +0530

    __init__

---
+++ sysmom/__init__.py
@@ -0,0 +1,5 @@
+"""
+SysMom CLI: Real-time system monitoring & resource dashboard.
+"""
+
+__version__ = "1.0.0"
--- sysmon/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-SysMon CLI: Real-time system monitoring & resource dashboard.
-"""
-
-__version__ = "1.0.0"
